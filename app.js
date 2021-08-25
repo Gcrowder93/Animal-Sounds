c69db62b578b0c2ec43bb89c7f6a49b109b1e8f2
Gcrowder93 <gcrowder93@gmail.com>
2021-08-25 16:13:35 -0700

    click for surprise

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-// import functions
+const emuImg = document.getElementById('emu');
+const emuAudio = document.getElementById('emu-sound');
+const sheepImg = document.getElementById('sheep');
+const sheepAudio = document.getElementById('sheep-sound');
+const whaleImg = document.getElementById('whale');
+const whaleAudio = document.getElementById('whale-sound');
 
-// reference needed DOM elements
+emuImg.addEventListener('click', () => {
+    emuAudio.play();
 
+});
 
-// set event listeners 
-  // get user input(s)
-  // do any needed work with the value(s)
-  // update DOM to reflect new value(s)
+sheepImg.addEventListener('click', () => {
+    sheepAudio.play();
+
+});
+
+whaleImg.addEventListener('click', () => {
+    whaleAudio.play();
+
+});
